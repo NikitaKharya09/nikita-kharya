7ef5f8d2d7437738ae172c052da17108b71ee8a9
NikitaKharya09 <46395532+NikitaKharya09@users.noreply.github.com>
2025-10-05 13:41:39 -0400

    Update webpack.config.js for tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,44 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/public"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html", // to import index.html file inside index.js
+      template: "index.html",
     }),
   ],
   devServer: {
-    port: 8080, // you can change the port
+    port: 8080,
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // .js and .jsx files
-        exclude: /node_modules/, // excluding the node_modules folder
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
       {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",  // ← ADD THIS for Tailwind
+          "sass-loader"
+        ],
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         loader: "url-loader",
         options: { limit: false },
       },
       {
         test: /\.(jpe?g|gif)$/i, 
         loader: 'file-loader',
-       
-    }
+      }
     ],
   },
 };
