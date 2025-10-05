f4fe10785af8807190d8c736aa2224a1da84b006
NikitaKharya09 <46395532+NikitaKharya09@users.noreply.github.com>
2025-10-05 13:03:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,45 +59,47 @@
 
 import React from 'react';
 
-import { About } from "./sections/about";
-import { Contact } from "./sections/contact";
-import { Personal } from "./sections/personal";
-import { Skills } from "./sections/skills";
-import { Parallax } from "react-scroll-parallax";
-import { Experience } from "./sections/experience";
+// import { About } from "./sections/about";
+// import { Contact } from "./sections/contact";
+// import { Personal } from "./sections/personal";
+// import { Skills } from "./sections/skills";
+// import { Parallax } from "react-scroll-parallax";
+// import { Experience } from "./sections/experience";
+import { MainComp } from "./sections/mainComp";
 
 import './App.scss';
-import { Nav } from './sections/navigation';
+// import { Nav } from './sections/navigation';
 
 const App = () => {
   return (
     <div className="home-container">
-      <Nav />
-      <section id="about" className="">
-        <div className="content">
-          <About />
-        </div>
-      </section>
-      <section id="skills">
-        <div className="content">
-          <Skills />
-        </div>
-      </section>
-      <section id="experience" className="">
-        <div className="content">
-          <Experience />
-        </div>
-      </section>
-      <section id="personal">
-        <div className="content">
-          <Personal />
-        </div>
-      </section>
-      <section id="contacts">
-        <div className="content">
-          <Contact />
-        </div>
-      </section>
+        <MainComp />
+      // <Nav />
+      // <section id="about" className="">
+      //   <div className="content">
+      //     <About />
+      //   </div>
+      // </section>
+      // <section id="skills">
+      //   <div className="content">
+      //     <Skills />
+      //   </div>
+      // </section>
+      // <section id="experience" className="">
+      //   <div className="content">
+      //     <Experience />
+      //   </div>
+      // </section>
+      // <section id="personal">
+      //   <div className="content">
+      //     <Personal />
+      //   </div>
+      // </section>
+      // <section id="contacts">
+      //   <div className="content">
+      //     <Contact />
+      //   </div>
+      // </section>
     </div>
   );
 };
