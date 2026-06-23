94bb959053009cc9290347316bb1d556b643e1d2
NikitaKharya09 <46395532+NikitaKharya09@users.noreply.github.com>
2026-06-23 09:33:25 -0400

    Comment out React imports and render function
    Comment out React and related imports, as well as the render function.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./App.scss";
-import { ParallaxProvider } from "react-scroll-parallax";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import "./App.scss";
+// import { ParallaxProvider } from "react-scroll-parallax";
 
-const el = document.getElementById("app");
+// const el = document.getElementById("app");
 
-ReactDOM.render(
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>,
-  el
-);
+// ReactDOM.render(
+//     <ParallaxProvider>
+//       <App />
+//     </ParallaxProvider>,
+//   el
+// );
